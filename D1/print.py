bbd513488f74bfcfe05c3096cd5cbb240ffce0b9
Nuthan Murarysetty <inbox.nuthan@gmail.com>
2021-05-10 15:06:08 +0000

    1. About printing statements in different ways 2. Learn string manipulation techniques

---
+++ D1/print.py
@@ -0,0 +1,17 @@
+# Write your code below this line 👇
+print("Day - 1 - Explained what, why, how print helps in python")
+
+print("What is print")
+
+print("1. print command is used to display on what we want to see in console window")
+
+print("Ex: If you want to see the name then ")
+print('Nani')
+
+print("2. All statements in python are Case-Sensitive Ex: p or P both are different.")
+
+print("3. You can print the statement either use '(Single Quote) or \"(Double Quote). Ex: 'Nani' or 'Potti'. ")
+
+print('4. Python follows no semi colon at the end of the each line and its not mandatory to follow.')
+
+print("5. If you type Print('something')then it throws an error: NameError: name 'Print' is not defined")
