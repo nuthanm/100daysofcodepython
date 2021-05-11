34c9e3aeb9b3a6e30709b95139664dd6160e30e7
Nuthan Murarysetty <inbox.nuthan@gmail.com>
2021-05-11 15:38:08 +0000

    Day 2 - Learning different data types

---
+++ Day2/datatypes.py
@@ -0,0 +1,12 @@
+# Python Data types
+'''
+In Python, main data types we use in our day to day are
+string, integer, float and boolean
+No need to define data type i.e., 123 by default treated as integers, 12.34 treated as float, "Nani" or 'Potti' treated as string, True or False treated as boolean
+'''
+# String - Presents data in between either 'text' or "text" => Series of charecters or collection of charecters
+print("Hello") # Hello is a string
+print("Potti"[0]) # returns P because we set position/Index in string to get the character (We call this as Subscript).
+# print("Potti"[5]) # This give index error
+
+# Integer
---
+++ overview.md
@@ -0,0 +1,6 @@
+# Day 2
+## Learning Objectives
+  1. Data Types
+  2. Type Conversion & Type Checking
+  3. Mathematical Operations
+
