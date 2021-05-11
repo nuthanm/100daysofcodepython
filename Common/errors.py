76b5b7ec285285bb2f4cf2ddc097662823dc0861
Nuthan Murarysetty <inbox.nuthan@gmail.com>
2021-05-11 21:07:25 +0530

    Updated few more errors information

--- Common/errors.py
+++ Common/errors.py
@@ -5,3 +5,5 @@
 #4. NameError: Ex: name 'Print' is not defined
 #5. Error: no "print" mailcap rules found for type "text/x-python Ex: Without any python function just type some text"
 #6. TypeError - Can only concatenate str (not "int" to str => Trying to add len to string)
+#7. TypeError - object of type 'int' has no len() => print(len(123))
+#8. IndexError - string index out of range => "Hello"[5] - It has only 5 charecters but expecting 6 character (Starts with 0 so that 5 means here 6)
