06cd89294ab558be3159e7b239d166f9bd7fe887
Nuthan Murarysetty <inbox.nuthan@gmail.com>
2021-05-11 23:35:18 +0530

    Added errors information for reference

--- Common/errors.py
+++ Common/errors.py
@@ -2,8 +2,12 @@
 #1. SyntaxError: Ex: unexpected EOF while parsing - It means either missed semicolon/bracket
 #2. SyntaxError: Ex: EOL while scanning string literal
 #3. IndentationError: Ex: Extra space added
-#4. NameError: Ex: name 'Print' is not defined
+#4. NameError: 
+    # Ex: name 'Print' is not defined
+    # Ex: name 'true' is not defined => True is acceptable.
+    
 #5. Error: no "print" mailcap rules found for type "text/x-python Ex: Without any python function just type some text"
 #6. TypeError - Can only concatenate str (not "int" to str => Trying to add len to string)
 #7. TypeError - object of type 'int' has no len() => print(len(123))
 #8. IndexError - string index out of range => "Hello"[5] - It has only 5 charecters but expecting 6 character (Starts with 0 so that 5 means here 6)
+#9. ValueError - Invalid literal for int() with base 10: 'n' => Passing input as 'nm' trying to parse int(nm[0])
