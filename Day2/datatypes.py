7e61172b5e81d876dc3c417165253070820a9fff
Nuthan Murarysetty <inbox.nuthan@gmail.com>
2021-05-11 17:35:59 +0000

    Code snippets for Data types and Type conversion and checking

--- Day2/datatypes.py
+++ Day2/datatypes.py
@@ -10,3 +10,33 @@ print("Potti"[0]) # returns P because we set position/Index in string to get the
 # print("Potti"[5]) # This give index error
 
 # Integer
+print(123)
+
+# Float
+print(3.14)
+print(29.20)
+
+# Boolean
+print(True)
+# Wrong print(true) statement
+
+# Type Error Example
+#  print('My age '+ 29 + 'years old')
+# Error gives as TypeError: can only concatenate str (not "int") to str
+
+# Type checking with type()
+print(type(23))
+
+# Type Conversion => To fix the above concatenate issue
+#str(), float(), int()
+print(str(123)) # 123
+print(str(143)+ " Potti") # 143 Potti
+print(int(10.25)) # convert float to int 10
+print(float(123)) # convert int to float 123.0
+print(bool(1)) # True
+print(bool(0)) # False
+print(bool(-2)) # True
+print(bool(-0)) # False
+
+
+
--- Day2/exam.py
+++ Day2/exam.py
@@ -1,44 +1,17 @@
+# Program 
 '''
-Find issues in the following programs and fix those
-----------------------------------------------------
+1. Take two digit number as an input
+2. Ex: 29
+3. Output: 2 + 9 = 11
 '''
+two_digit_number = input(" Enter two digit number ")
 
-# Program 1 - Find how many errors in following line
-'''
-Dont change any line just copy the print statement including space. 
-First identify the statement clearly and verify after.
-
-1. print(Find issue in this line)
-
-2. Print('What would be the issue with this print')
-
-3. print("Is this statement correct?")
-
-4.  print('This will execute or error")
-
-5. print("Hello!!!!" + "Potti") => What is the output?
-
-6. print('Hello: ' + input('Please enter your like? ')) => What is the output for this statement?
-'''
-
-# Program 2 - Take two inputs and assign into a variables. Swap those variables and print them back. Ex: a = 10, b = 20 then output should be a = 20 and b = 10
-a = input('Value of a: ')
-b = input('Value of b: ')
-
-print("Actual value of a :" + a)
-print("Actual value of b :" + b)
-
-# Swap here using 2 variables.
-a = b
-b = 
-
-
-
-# Swap here using 3 variables.
-temp = a # temp = 10
-a = b # a = 20
-b = temp # b = 10
-
-print("Actual value of a :" + a)
-print("Actual value of b :" + b)
-
+if len(two_digit_number) > 2:
+  print('Input length crosses 2 digit')
+else:  
+  first_digit = int(two_digit_number[0])
+  second_digit = int(two_digit_number[1])
+  total = first_digit + second_digit
+  print('First digit' + str(first_digit))
+  print('Second digit' + str(second_digit))
+  print('Total digit value ' + str(total))
